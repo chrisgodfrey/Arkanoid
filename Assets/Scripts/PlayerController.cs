0552ec9de831ee6a5506db8dbd2c421ee66ce462
Chris Godfrey <chris@cgodfrey.net>
2019-02-01 00:37:10 +0000

    Fixed background

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 30;
+    public string axis = "Horizontal";
+
+    void FixedUpdate ()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(v, 0) * speed;
+    }
+}
