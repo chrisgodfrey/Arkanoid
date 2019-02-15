a4d9c4a6585006a4b232d2d4f2c3bbfc1face483
Chris Godfrey <chris@cgodfrey.net>
2019-02-15 12:29:57 +0000

    Added basic menu system, credits & instructions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Loading One Player game");
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void HowToPlay()
+    {
+        Debug.Log("Displaying How To Play Scene");
+        SceneManager.LoadScene("HowToPlay", LoadSceneMode.Single);
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Displaying Credits Scene");
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("Returning to main menu");
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit(); // quit game to OS (ignored in Unity Editor)
+    }
+}
